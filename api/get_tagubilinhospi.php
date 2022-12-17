486dec11566fe3ee4e9e4d63c7bdf53bca2a0daf
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-12-17 10:01:41 +0800

    krizelle

--- api/get_tagubilinhospi.php
+++ api/get_tagubilinhospi.php
@@ -11,11 +11,11 @@ switch($method){
 
         $sql = "SELECT * FROM tagubilin LEFT JOIN result ON tagubilin.FK_resultId=result.PK_resultId LEFT JOIN follow_up ON result.PK_resultId=follow_up.PK_followupId";
         $path= explode('/', $_SERVER['REQUEST_URI']);
-        if(isset($path[3]) && is_numeric($path[3])) {
-        $sql.="WHERE tagubilin.PK_tagubilinId=:id";
+        if(isset($path[4]) && is_numeric($path[4])) {
+        $sql.="WHERE tagubilin.PK_tagubilinId=:id"; 
 
         $stmt = $db->prepare($sql);
-        $stmt->bind_param(':id', $path[3]);
+        $stmt->bind_param(':id', $path[4]);
         $stmt->execute();
         $data = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
         }else{
